367a539611d8b38659c322be8d6cbd7df119c036
dibyarupajena <dibyathecool.jena@gmail.com>
2024-05-03 00:23:15 +0530

    Non-easy example-2 of CardWrappers, SORRY did empty commit earlier

--- week-6-exps/src/App.jsx
+++ week-6-exps/src/App.jsx
@@ -1,27 +1,27 @@
 function App() {
-  return <div>
-  <CardWrapper innerComponent={<TextComponent />} />
-  <CardWrapper innerComponent={<TextComponent2 />} />
-  </div>
-  
-  }
-  
-  function CardWrapper({innerComponent}) {
-  // and inside the div, renders the prop
-  return <div style={{border: "2px solid black"}}>
-  {innerComponent}
-  </div>
-  }
 
-  function TextComponent () {
-  return <div>
-  hi there
-  </div>
-  }
-  function TextComponent2 () {
-    return <div>
-    hi there but I'm from another div but I have the same wrapper wrapped around me!!!
+  return (
+    <div style={{display: "flex"}}>
+      <Card>
+        hi there
+      </Card>
+      <Card>
+        <div>
+          hello from the 2nd card
+        </div>
+      </Card>
     </div>
-    }
+  )
+}
 
-  export default App
+function Card({children}) {
+  return <div style={{
+    border: "1px solid black",
+    padding: 10,
+    margin: 10
+  }}>
+    {children}
+  </div>
+}
+
+export default App
