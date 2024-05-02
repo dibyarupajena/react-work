6eee467b04dc229efba09ad983aa08ebf4106b28
dibyarupajena <dibyathecool.jena@gmail.com>
2024-05-02 23:22:38 +0530

    Another Answer to the last re-rendering problem- react.memo, explanation in the comments

--- week-6-exps/src/App.jsx
+++ week-6-exps/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react"
+import { memo } from 'react';
 
-//do npm install then npm run dev to see the project
-// using react dev tools see how these components vs the previous git commit version 
-//re-renders and if this solves the problem of unneccesary re-renderings in childs or not
 function App() {
+  const [firstTitle, setFirstTitle] = useState("my name is harkirat");
+
+  function changeTitle() {
+    setFirstTitle("My name is " + Math.random())
+  }
+
   return (
     <div>
-      <HeaderWithButton />
+      <button onClick={changeTitle}>Click me to change the title</button>
+      <Header title={firstTitle} />
+      <br />
       <Header title="My name is raman" />
       <Header title="My name is raman" />
       <Header title="My name is raman" />
@@ -15,25 +21,12 @@ function App() {
   )
 }
 
-//wherever you keep the state variables, that function will re-render(along with any child components within it)
-//so keep the state component separate with the one components needing re-rendereing as a separate function
-function HeaderWithButton() {
-  const [firstTitle, setFirstTitle] = useState("my name is harkirat");
-
-  function changeTitle() {
-    setFirstTitle("My name is " + Math.random())
-  }
-
-  return <>
-    <button onClick={changeTitle}>Click me to change the title</button>
-    <Header title={firstTitle} />
-  </>
-}
-
-function Header({title}) {
+//react.memo re-renders just the components with a dynamic prop{title} in this case
+// in this case, only the first <Header> re-renders above since it has dynamic props or state variables' effects
+const Header = memo(function ({title}) {
   return <div>
     {title}
   </div>
-}
+})
 
 export default App
