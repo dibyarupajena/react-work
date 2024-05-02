b1dac4fdb18d571b39e7c3345e0973e9fae7a5ec
dibyarupajena <dibyathecool.jena@gmail.com>
2024-05-02 22:35:04 +0530

    Initial commit, math.random re-rendering

---
+++ week-6-exps/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+function App() {
+  const [firstTitle, setFirstTitle] = useState("my name is harkirat");
+
+  function changeTitle() {
+    setFirstTitle("My name is " + Math.random())
+  }
+
+  return (
+    <div>
+      <button onClick={changeTitle}>Click me to change the title</button>
+      <Header title={firstTitle} />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+      <Header title="My name is raman" />
+    </div>
+  )
+}
+
+function Header({title}) {
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
