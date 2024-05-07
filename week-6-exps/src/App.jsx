caa7a84fa5bef0964c28a55691e3e10b1599627e
dibyarupajena <dibyathecool.jena@gmail.com>
2024-05-07 15:54:38 +0530

    another way of avoiding continous re-rendering, using useEffects

--- week-6-exps/src/App.jsx
+++ week-6-exps/src/App.jsx
@@ -1,27 +1,38 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function App() {
+  const [todos, setTodos] = useState([])
 
-  return (
-    <div style={{display: "flex"}}>
-      <Card>
-        hi there
-      </Card>
-      <Card>
-        <div>
-          hello from the 2nd card
-        </div>
-      </Card>
-    </div>
-  )
-}
+  // fetch("https://sum-server.100xdevs.com/todos")
+  // .then(async function(res) {
+  //   const json = await res.json();
+  //   setTodos(json.todos); // this will cause infinite looping, hence infinite rendering, since setTodos is called so the state variable changes, the parent function 
+  // App() //gets rendered once again and again starts from line 5 and comes to this line and again this process repeats
+  // })
+  // another way of avoiding continous re-rendering, using useEffects
+  useEffect(() => {
+    fetch("https://sum-server.100xdevs.com/todos")
+      .then(async function(res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+  }, []) // the second argument is called dependency array
 
-function Card({children}) {
-  return <div style={{
-    border: "1px solid black",
-    padding: 10,
-    margin: 10
-  }}>
-    {children}
+  return <div>
+    {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
   </div>
 }
 
-export default App
+function Todo({title, description}) {
+  return <div>
+    <h1>
+      {title}
+    </h1>
+    <h4>
+      {description}
+    </h4>
+  </div>
+}
+
+export default App;
