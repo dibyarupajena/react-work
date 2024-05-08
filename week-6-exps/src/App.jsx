113d7ae3b475aec8ce65e1f72420016f7dacaab4
dibyarupajena <dibyathecool.jena@gmail.com>
2024-05-09 00:45:48 +0530

    another examples of  using useEffects to call apis(id specific)

--- week-6-exps/src/App.jsx
+++ week-6-exps/src/App.jsx
@@ -1,36 +1,33 @@
+//trying the previous code(getting data from an api) using
+//useEffect() but this we are getting it some a different api url
+// i.e where we need to send the todo id and get results based on it
+
 import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
-  const [todos, setTodos] = useState([])
-
-  // fetch("https://sum-server.100xdevs.com/todos")
-  // .then(async function(res) {
-  //   const json = await res.json();
-  //   setTodos(json.todos); // this will cause infinite looping, hence infinite rendering, since setTodos is called so the state variable changes, the parent function 
-  // App() //gets rendered once again and again starts from line 5 and comes to this line and again this process repeats
-  // })
-  // another way of avoiding continous re-rendering, using useEffects
-  useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todos")
-      .then(async function(res) {
-        const json = await res.json();
-        setTodos(json.todos);
-      })
-  }, []) // the second argument is called dependency array
-
   return <div>
-    {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+    <Todo id={3} />
   </div>
 }
 
-function Todo({title, description}) {
+function Todo({id}) {
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    fetch("https://sum-server.100xdevs.com/todo?id=" + id)
+      .then(async function(res) {
+        const json = await res.json();
+        setTodo (json.todo);
+      })
+  }, [])
+
   return <div>
     <h1>
-      {title}
+      {todo.title}
     </h1>
     <h4>
-      {description}
+      {todo.description}
     </h4>
   </div>
 }
