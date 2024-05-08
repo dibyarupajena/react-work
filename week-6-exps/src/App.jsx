08efd40fd8595c9ac4a42a083ca4c2ccd8771b25
dibyarupajena <dibyathecool.jena@gmail.com>
2024-05-09 02:51:36 +0530

    2.3.1 Problems with useEffect return error

--- week-6-exps/src/App.jsx
+++ week-6-exps/src/App.jsx
@@ -1,19 +1,27 @@
 // solvong the previous problem with useMemo
 // Another difference is that useEffect runs after rendering, while useMemo runs during rendering.
 // thats why setFinalValue- state variable was needed for useEffect but not needed here for useMemo, returns works just fine
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [inputValue, setInputValue] = useState(1);
  
 
-  let finalValue = useMemo(() => {
-      let sum = 0;
-      for( let i = 1; i<= inputValue; i++){
-        sum = sum + i;
-      }
-      return sum; 
+  const doSomething = () => {
+    let sum = 0;
+    for( let i = 1; i<= inputValue; i++){
+      sum = sum + i;
+    }
+    return sum; 
+  }
+  let finalValue = useEffect(() => {
+      doSomething();
+      // let sum = 0;
+      // for( let i = 1; i<= inputValue; i++){
+      //   sum = sum + i;
+      // }
+      // return sum; 
   }, [inputValue])                                    
 
   return <div>
