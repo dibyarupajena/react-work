fd2f353cacd8e6a4aea87a6dbf481a9e8785bd0f
dibyarupajena <dibyathecool.jena@gmail.com>
2024-05-09 02:24:59 +0530

    Same problem statement solved without useMemo but with useEffect(i.e to lessen the renders)

--- week-6-exps/src/App.jsx
+++ week-6-exps/src/App.jsx
@@ -1,24 +1,30 @@
-// useful just for performance, 
-// useMemo- dont recompute- memoization
+// solvong the previous problem with useEffects
+// demerits- extra state variable required, one extra render
+//when one state variable is dependent directly like in on another
+//state variable, best is to use useMemo
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [inputValue, setInputValue] = useState(1);
+  const [finalValue, setFinalValue] = useState(0);
+ 
 
-
-  let sum = 0;
-  for (let i = 1; i <= inputValue; i++) {
-    sum = sum + i;
-  }
+  useEffect(() => {
+      let sum = 0;
+      for( let i = 1; i<= inputValue; i++){
+        sum = sum + i;
+      }
+      setFinalValue(sum); //do without this? no, needed to change a global variable
+  }, [inputValue])                                    
 
   return <div>
     <input onChange={function(e) {
       setInputValue(e.target.value);
     }} placeholder={"Find sum from 1 to n"}></input>
     <br />
-    Sum from 1 to {inputValue} is {sum}
+    Sum from 1 to {inputValue} is {finalValue}
     <br />
     <button onClick={() => {
       setCounter(counter + 1);
